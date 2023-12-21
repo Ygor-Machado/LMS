39cc0eff505cb1c4d4fff338d99d6b15e55927ec
Ygor-Machado <ygaounderground011@gmail.com>
2023-12-21 11:39:33 -0300

    feat: atualizar imagem instrutor

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,5 +28,41 @@ class InstructorController extends Controller
     public function InstructorLogin()
     {
         return view('instructor.instructor_login');
+    }
+
+    public function InstructorProfile()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+
+        return view('instructor.instructor_profile_view', compact('profileData'));
+    }
+
+    public function InstructorProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        $profileData->name = $request->name;
+        $profileData->username = $request->username;
+        $profileData->email = $request->email;
+        $profileData->phone = $request->phone;
+        $profileData->address = $request->address;
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/instructor_images/'.$profileData->photo));
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/instructor_images'), $filename);
+            $profileData['photo'] = $filename;
+        }
+
+        $profileData->save();
+
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
     }
 }
